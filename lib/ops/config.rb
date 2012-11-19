e0dfa4ff61ae37c9efe590a636c487c6d42fb2ef
Colin Rymer <colin.rymer@gmail.com>
2012-11-19 11:49:44 -0500

    setting up the basics

---
+++ lib/ops/config.rb
@@ -0,0 +1,43 @@
+module Ops
+  class Config
+    def initialize(data={})
+      @data = {}
+      update!(data)
+    end
+
+    def update!(data)
+      data.each do |key, value|
+        self[key] = value
+      end
+    end
+
+    def [](key)
+      @data[key.to_sym]
+    end
+
+    def []=(key, value)
+      if value.class == Hash
+        @data[key.to_sym] = Config.new(value)
+      else
+        @data[key.to_sym] = value
+      end
+    end
+
+    def method_missing(sym, *args)
+      if sym.to_s =~ /(.+)=$/
+        self[$1] = args.first
+      else
+        self[sym]
+      end
+    end
+  end
+
+  class << self
+    attr_accessor :config
+
+    def setup
+      self.config ||= Config.new
+      yield config
+    end
+  end
+end
