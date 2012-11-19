378edc5975bef1bc1af9547c54817a0626eeb06a
Michael Pelz-Sherman <mpelzsherman@primedia.com>
2012-11-19 15:51:46 -0500

    clean up our mess

--- spec/ops/revision_spec.rb
+++ spec/ops/revision_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 describe Ops::Revision do
 
   before do
-    root = File.dirname(__FILE__)
-    settings = double("settings", {:file_root => root, :environment => 'test'})
-    File.open(File.join(root, 'VERSION'), 'w') do |f|
-      f.write('v23.24.25^{}')
-    end
+    @root = File.dirname(__FILE__)
+    @version_file = File.join(@root, 'VERSION')
+    settings = double("settings", {:file_root => @root, :environment => 'test'})
+    File.open(@version_file, 'w') { |f| f.write('v23.24.25^{}') }
     headers = {}
     @version = Ops::Revision.new(headers, settings)
+  end
+
+  after do
+    File.delete(@version_file)
   end
 
   it "should have version" do
